db7c7dbedaa293caee4165ea60a65854b231a374
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-12 18:47:57 +0500

    Создал красный тест для метода GeneratePoints

--- cs/TagsCloudVisualizationTests/FermatSpiralPointsGeneratorTest.cs
+++ cs/TagsCloudVisualizationTests/FermatSpiralPointsGeneratorTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Drawing;
+using System.Linq;
 using NUnit.Framework;
 using TagsCloudVisualization;
 using FluentAssertions;
@@ -20,4 +22,25 @@ public class FermatSpiralPointsGeneratorTest
         creation.Should().Throw<ArgumentException>();
         
     }
+
+    [TestCaseSource(nameof(GeneratePointsTestCases))]
+    public Point GeneratePoints_ShouldReturnCorrectPoint(double radius, double angleOffset, int pointNumber)
+    {
+        var pointsGenerator = new FermatSpiralPointsGenerator(radius, angleOffset);
+        var actualPoint = pointsGenerator
+            .GeneratePoints(new Point(0, 0))
+            .Skip(pointNumber)
+            .First();
+        return actualPoint;
+    }
+
+    public static TestCaseData[] GeneratePointsTestCases =
+    {
+        new TestCaseData(1, 125, 0).Returns(new Point(0, 0)),
+        new TestCaseData(10, 125, 1).Returns(new Point(-2, 3)),
+        new TestCaseData(1, 360, 1).Returns(new Point(1, 0)),
+        new TestCaseData(2, 180, 1).Returns(new Point(-1, 0)),
+        new TestCaseData(4, 90, 1).Returns(new Point(0, 1)),
+        new TestCaseData(4, 300, 1).Returns(new Point(2, -3))
+    };
 }
