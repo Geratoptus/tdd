a845257fc12bea5c3896ce095149c06015b7e729
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-12 19:31:23 +0500

    Создал CircularCloudLayouterTest.cs с красным тестом для метода PutNextRectangle

---
+++ cs/TagsCloudVisualizationTests/CircularCloudLayouterTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+[TestOf(typeof(CircularCloudLayouter))]
+public class CircularCloudLayouterTest
+{
+
+    [Test]
+    [Repeat(10)]
+    public void PutNextRectangle_ShouldReturnRectanglesWithoutIntersections()
+    {
+        var randomizer = new Random();
+        var rectanglesNumber = randomizer.Next(100, 250);
+        var rectangles = new Rectangle[rectanglesNumber];
+        var circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0), 1, 0.5);
+        
+        for (var i = 0; i < rectanglesNumber; i++)
+        {
+            var width = randomizer.Next(10, 25);
+            var height = randomizer.Next(10, 25);
+            var size = new Size(width, height);
+            
+            rectangles[i] = circularCloudLayouter.PutNextRectangle(size);
+        }
+
+        IsIntersectionBetweenRectangles(rectangles).Should().BeFalse();
+    }
+
+    private bool IsIntersectionBetweenRectangles(Rectangle[] rectangles)
+    {
+        for (var i = 0; i < rectangles.Length; i++)
+        {
+            for (var j = i + 1; j < rectangles.Length; j++)
+            {
+                if (rectangles[i].IntersectsWith(rectangles[j]))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
