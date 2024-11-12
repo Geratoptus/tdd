0a995979d217593f146127e0929b4b7c650c34f2
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-12 18:48:52 +0500

    Теперь тест для метода GeneratePoints зеленый

--- cs/TagsCloudVisualization/FermatSpiralPointsGenerator.cs
+++ cs/TagsCloudVisualization/FermatSpiralPointsGenerator.cs
@@ -4,17 +4,40 @@ namespace TagsCloudVisualization;
 
 public class FermatSpiralPointsGenerator : IPointsGenerator
 {
+    private readonly double angleOffset;
+    private readonly double radius;
+    
+    private double OffsetPerRadian => radius / (2 * Math.PI);
+
     public FermatSpiralPointsGenerator(double radius, double angleOffset)
     {
         if (radius <= 0)
             throw new ArgumentException("radius must be greater than 0", nameof(radius));
         if (angleOffset <= 0)
             throw new ArgumentException("angleOffset must be greater than 0", nameof(angleOffset));
-        
+        this.angleOffset = angleOffset * Math.PI / 180;
+        this.radius = radius;
     }
 
     public IEnumerable<Point> GeneratePoints(Point spiralCenter)
     {
-        yield return spiralCenter;
+        double angle = 0;
+
+        while (true)
+        {
+            yield return GetPointByPolarCoordinates(spiralCenter, angle);
+
+            angle += angleOffset;
+        }
+    }
+
+    private Point GetPointByPolarCoordinates(Point spiralCenter, double angle)
+    {
+        var radiusVector = OffsetPerRadian * angle;
+        
+        var x = (int)Math.Round(radiusVector * Math.Cos(angle) + spiralCenter.X);
+        var y = (int)Math.Round(radiusVector * Math.Sin(angle)  + spiralCenter.Y);
+        
+        return new Point(x , y);
     }
 }
