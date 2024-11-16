892ddff4a730edb86dc2d0252c561c2b6262f33f
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-16 15:10:13 +0500

    Добавил заготовку для RandomExtension.cs

---
+++ cs/TagsCloudVisualization/Extensions/RandomExtension.cs
@@ -0,0 +1,11 @@
+using System.Drawing;
+
+namespace TagsCloudVisualization.Extensions;
+
+public static class RandomExtension
+{
+    public static Size RandomSize(this Random random, int minValue, int maxValue)
+    {
+        throw new NotImplementedException();
+    }
+}
