e36483ee27bae2b4f50c1194241ca9455ea529b3
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-16 23:02:11 +0500

    IVisualizer.cs CreateBitmap теперь не закрепляет в поведении зависимость метода от bitmapSize

--- cs/TagsCloudVisualization/Visualizers/IVisualizer.cs
+++ cs/TagsCloudVisualization/Visualizers/IVisualizer.cs
@@ -3,5 +3,5 @@ using System.Drawing;
 
 public interface IVisualizer
 {
-    public Bitmap CreateBitmap(IEnumerable<Rectangle> rectangles, Size bitmapSize);
+    public Bitmap CreateBitmap(IEnumerable<Rectangle> rectangles);
 }
