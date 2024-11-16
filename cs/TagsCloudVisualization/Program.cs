7406a834b09bf855ee539a9155515f022c9c6a35
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-16 23:33:14 +0500

    Program.cs -Внедрение нового конструктора DefaultVisualizer -Теперь имя файла не зависит от locale

--- cs/TagsCloudVisualization/Program.cs
+++ cs/TagsCloudVisualization/Program.cs
@@ -1,5 +1,6 @@
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Globalization;
 using TagsCloudVisualization.CloudLayouters;
 using TagsCloudVisualization.Extensions;
 using TagsCloudVisualization.Visualizers;
@@ -16,29 +17,30 @@ public static class Program
     private const int MaxRectangleSize = 50;
     
     private const double LayoutRadius = 1;
-    private const double LayoutAngleOffset = 10;
+    private const double LayoutAngleOffset = 0.5;
 
     private const string ImagesDirectory = "images";
     
     public static void Main()
     {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        
         var center = new Point(ImageWidth / 2, ImageHeight / 2);
         var cloudLayouter = new CircularCloudLayouter(center, LayoutRadius, LayoutAngleOffset);
         var random = new Random();
-        var rectangles = new Rectangle[RectanglesNumber];
-
-        rectangles = rectangles
+        var rectangles = Enumerable
+            .Range(0, RectanglesNumber)
             .Select(_ => cloudLayouter.PutNextRectangle(random.RandomSize
                 (MinRectangleSize, MaxRectangleSize)))
             .ToArray();
         
-        var visualizer = new DefaultVisualizer();
-        var bitmap = visualizer.CreateBitmap(rectangles, new Size(ImageWidth, ImageHeight));
+        var visualizer = new DefaultVisualizer(new Size(ImageWidth, ImageHeight));
+        var bitmap = visualizer.CreateBitmap(rectangles);
         Directory.CreateDirectory(ImagesDirectory);
         
         bitmap.Save(GetPathToImages(), ImageFormat.Jpeg);
     }
-
+    
     private static string GetPathToImages()
     {
         var filename = $"{RectanglesNumber}_{LayoutRadius}_{LayoutAngleOffset}_TagCloud.jpg";
