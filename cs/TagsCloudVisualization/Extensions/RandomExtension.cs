695e7bb9e3f985b69681d60c9b8d2a1f491fa898
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-16 15:34:11 +0500

    RandomExtension.cs RandomSize теперь тест зеленый

--- cs/TagsCloudVisualization/Extensions/RandomExtension.cs
+++ cs/TagsCloudVisualization/Extensions/RandomExtension.cs
@@ -6,6 +6,10 @@ public static class RandomExtension
 {
     public static Size RandomSize(this Random random, int minValue, int maxValue)
     {
-        throw new NotImplementedException();
+        if (minValue <= 0)
+            throw new ArgumentOutOfRangeException(nameof(minValue), "minValue must be positive");
+        if (minValue > maxValue)
+            throw new ArgumentOutOfRangeException(nameof(minValue), "minValue must be less than maxValue");
+        return new Size();
     }
 }
