5dd291ec8b41042da2f3fff7db2b3155ed288933
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-17 14:37:46 +0500

    DefaultVisualizer.cs добавил оцентровку изображения

--- cs/TagsCloudVisualization/Visualizers/DefaultVisualizer.cs
+++ cs/TagsCloudVisualization/Visualizers/DefaultVisualizer.cs
@@ -4,17 +4,30 @@ namespace TagsCloudVisualization.Visualizers;
 
 public class DefaultVisualizer(Size bitmapSize) : IVisualizer
 {
+    private Point bitmapCenter = new(bitmapSize.Width / 2, bitmapSize.Height / 2);
+    
     public Bitmap CreateBitmap(IEnumerable<Rectangle> rectangles)
     {
         var bitmap = new Bitmap(bitmapSize.Width, bitmapSize.Height);
+        
+        var deltaWidth = bitmapCenter.X - rectangles.First().X;
+        var deltaHeight = bitmapCenter.Y - rectangles.First().Y;
         
         using var graphics = Graphics.FromImage(bitmap);
         foreach (var rectangle in rectangles)
         {
             var pen = new Pen(Color.DeepPink);
-            graphics.DrawRectangle(pen, rectangle);
+            graphics.DrawRectangle(pen, 
+                CenterRectangle(rectangle, deltaWidth, deltaHeight));
         }
 
         return bitmap;
+    }
+
+    private Rectangle CenterRectangle(Rectangle rectangle, int deltaWidth, int deltaHeight)
+    {
+        var centeredRectanglePos = new Point(rectangle.Location.X + deltaWidth, 
+            rectangle.Location.Y + deltaHeight);
+        return new Rectangle(centeredRectanglePos, rectangle.Size);
     }
 }
